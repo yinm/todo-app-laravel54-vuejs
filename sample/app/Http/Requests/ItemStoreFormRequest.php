907dcf29eeb4550ed7dcf9ef2e72af66f3a104d9
yinm <yinm.cminor9@gmail.com>
2017-09-23 22:30:45 +0900

    Disable authorization

--- sample/app/Http/Requests/ItemStoreFormRequest.php
+++ sample/app/Http/Requests/ItemStoreFormRequest.php
@@ -13,7 +13,7 @@ class ItemStoreFormRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
--- sample/app/Http/Requests/ItemUpdateFormRequest.php
+++ sample/app/Http/Requests/ItemUpdateFormRequest.php
@@ -13,7 +13,7 @@ class ItemUpdateFormRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
