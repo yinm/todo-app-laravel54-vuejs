cfbc316e18317c8e352cf6e0e3ea54f976fd23ab
yinm <yinm.cminor9@gmail.com>
2017-09-23 21:53:49 +0900

    Add feature tests

--- sample/tests/Feature/ItemTest.php
+++ sample/tests/Feature/ItemTest.php
@@ -2,18 +2,73 @@
 
 namespace Tests\Feature;
 
+use App\Item;
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 class ItemTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testExample()
+    use DatabaseMigrations;
+
+    protected function setUp()
     {
-        $this->assertTrue(true);
+        parent::setUp();
+        (new \DatabaseSeeder())->run();
+    }
+
+    public function testIndex()
+    {
+        $response = $this->get('/api/items');
+
+        $response->assertStatus(200);
+        $this->assertCount(1, $response->json());
+    }
+
+    public function testShow()
+    {
+        $response = $this->get('/api/items/1');
+
+        $response->assertStatus(200);
+    }
+
+    public function testStore()
+    {
+        $data = ['content' => 'ブログを書く'];
+        $response = $this->post('/api/items', $data);
+
+        $response->assertStatus(201);
+        $response->assertJson($data);
+        $item = Item::query()->find($response->json()['id']);
+        $this->assertInstanceOf(Item::class, $item);
+    }
+
+    public function testUpdateContent()
+    {
+        $data = ['content' => 'ブログを書く'];
+        $response = $this->patch('/api/items/1', $data);
+
+        $response->assertStatus(200);
+        $response->assertJson($data);
+        $item = Item::query()->find(1);
+        $this->assertSame('ブログを書く', $item->content);
+    }
+
+    public function testUpdateChecked()
+    {
+        $data = ['checked' => 1];
+        $response = $this->patch('/api/items/1', $data);
+
+        $response->assertStatus(200);
+        $response->assertJson($data);
+        $item = Item::query()->find(1);
+        $this->assertEquals(true, $item->checked);
+    }
+
+    public function testDelete()
+    {
+        $response = $this->delete('/api/items/1');
+
+        $response->assertStatus(200);
+        $this->assertNull(Item::query()->find(1));
     }
 }
